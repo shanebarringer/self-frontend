3daab7ebf4d5ab00d566ad30233b1bdd8c4efe49
Shane Barringer <shane.barringer@outlook.com>
2019-11-20 22:33:19 -0600

    Updates

---
+++ precache-manifest.f3f046d17577694392365ffb6c8b13a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7593a5f090e9e2c0e760640c5ae033ee",
+    "url": "/self-frontend/index.html"
+  },
+  {
+    "revision": "26ef4f4b54ce4dd70971",
+    "url": "/self-frontend/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "298cca06aeab50ba70ff",
+    "url": "/self-frontend/static/css/main.725eb1a0.chunk.css"
+  },
+  {
+    "revision": "26ef4f4b54ce4dd70971",
+    "url": "/self-frontend/static/js/2.3f17993c.chunk.js"
+  },
+  {
+    "revision": "298cca06aeab50ba70ff",
+    "url": "/self-frontend/static/js/main.33f425aa.chunk.js"
+  },
+  {
+    "revision": "81a296e95d688bde82f2",
+    "url": "/self-frontend/static/js/runtime-main.953e1684.js"
+  },
+  {
+    "revision": "e533bd921f69f31de60372c8f5bb92c4",
+    "url": "/self-frontend/static/media/GiantRobotLTD_Logo.e533bd92.svg"
+  },
+  {
+    "revision": "7ef17c6949979ce35b79701690cc0e9c",
+    "url": "/self-frontend/static/media/White_Arrow.7ef17c69.svg"
+  }
+]);
