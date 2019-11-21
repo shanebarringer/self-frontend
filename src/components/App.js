4765e55c222da507bd1982e036669ed786e1266f
Shane Barringer <shane.barringer@outlook.com>
2019-11-20 20:17:39 -0600

    adds styles for inputs and header

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
-import React from "react";
 import Form from "./Form";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import "../styles/App.css";
 
 function App() {
   return (
-    <div>
-      <div>
+    <div className="App">
+      <div css={welcome}>
         <h1>Welcome</h1>
         <p>Please tell us a bit about yourself to get started</p>
       </div>
-      <Form />
+      <div css={formStyle}>
+        <Form />
+      </div>
     </div>
   );
 }
+
+const welcome = css`
+  background-color: #585858;
+  font-size: 14px;
+  color: #ffffff;
+  padding: 2em;
+  h1 {
+    font-size: 24px;
+    font-weight: 700;
+    font-family: Roboto, sans-serif;
+  }
+`;
+
+const formStyle = css`
+  margin: 12px auto;
+  margin-left: 10%;
+  margin-right: auto;
+`;
 
 export default App;
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,24 +1,6 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
-const colors = {
-  label: "#666666",
-  input: "#OOADEE",
-  idle: "#F7F7F7",
-  err: "#E70000"
-};
-
-const error = css`
-  border: 1px solid ${colors.err};
-`;
-
-const errorText = css`
-  ::after {
-    content: " REQUIRED";
-    color: ${colors.err};
-  }
-`;
-
 const FormInput = ({ val, submit, labelText, name, handleFormUpdate }) => {
   const err = submit && !val.length;
 
@@ -32,11 +14,48 @@ const FormInput = ({ val, submit, labelText, name, handleFormUpdate }) => {
         type="text"
         name={name}
         value={val}
-        css={err ? error : ""}
+        css={[inputBase, err ? error : ""]}
+        style={{ background: val.length ? "#fff" : "" }}
         onChange={handleFormUpdate}
       />
     </div>
   );
 };
+
+const colors = {
+  border: "#00ADEE",
+  label: "#666666",
+  idle: "#F7F7F7",
+  err: "#E70000"
+};
+
+const inputBase = css`
+  background-color: ${colors.idle};
+  font: 14px Roboto;
+  color: ${colors.label};
+  margin: 12px auto;
+  padding-left: 16px;
+  width: 80%;
+  height: 48px;
+  border-radius: 6px;
+  border: 1px solid ${colors.idle};
+  &:focus {
+    outline: 0;
+    border: 1px solid ${colors.border};
+    background-color: #fff;
+  }
+`;
+
+const error = css`
+  border: 1px solid ${colors.err};
+  background-color: #fff;
+`;
+
+const errorText = css`
+  ::after {
+    content: " REQUIRED";
+    color: ${colors.err};
+  }
+`;
 
 export default FormInput;
